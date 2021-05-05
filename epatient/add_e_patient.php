f918a7fb570d15e4d644a32a09c7c6ef79c86c64
root <gurunadh.pg123@gmail.com>
2021-05-05 20:19:58 +0530

    crud

---
+++ epatient/add_e_patient.php
@@ -0,0 +1,38 @@
+<html>
+    <body>
+        <?php
+	        echo '<body style="background-color:#383A59; color:white">';
+            $PATIENTID=$_POST["id"];
+            $PATIENTNAME=$_POST["name"];
+            $PATIENTADDRESS=$_POST["address"];
+            $PATIENTCONTACT=$_POST["contact_number"];
+            $PATIENTGENDER=$_POST["gender"];
+            try {
+                $username = "root";
+                $password = "guru";
+                $database = "hospital";
+                $table = "emergency_patient";
+                $mysqli = new mysqli("localhost", $username, $password, $database);
+                $query = "INSERT INTO emergency_patient (id, name, address, contact_number, gender) VALUES ('$PATIENTID', '$PATIENTNAME', '$PATIENTADDRESS', '$PATIENTCONTACT', '$PATIENTGENDER')";
+                $result = $mysqli->query($query);
+                if ($result == 1){
+                    echo "successfully added<br>";
+                    echo "$PATIENTID<br>";
+                    echo "$PATIENTNAME<br>";
+                    echo "$PATIENTADDRESS<br>";
+                    echo "$PATIENTCONTACT<br>";
+                    echo "$PATIENTGENDER<br>";
+                    echo '<form method="post" action="epatient.php">
+                        <input type="submit" value="go back">
+                      </form>';
+                } else {
+                    echo "FAILED TO ADD DATA<br>";
+                }
+                
+            } catch (mysqli_sql_exception $e) {
+                print "Error!: " . $e->getMessage() . "<br/>";
+                die();
+            }
+        ?>
+    </body>
+</html> 
---
+++ epatient/delete_e_patient.php
@@ -0,0 +1,35 @@
+<html>
+    <body>
+        <?php
+	        echo '<body style="background-color:#383A59; color:white">';
+            $PATIENTID=$_POST["id"];
+            try {
+                $username = "root";
+                $password = "guru";
+                $database = "hospital";
+                $table = "emergency_patient";
+                $mysqli = new mysqli("localhost", $username, $password, $database);
+                $query = "delete from emergency_patient where id = '$PATIENTID'";
+                $result = $mysqli->query($query);
+                if ($result == 1){
+                    echo "successfully deleted<br>";
+                    echo "$PATIENTID<br>";
+                    echo "$PATIENTNAME<br>";
+                    echo "$PATIENTADDRESS<br>";
+                    echo "$PATIENTCONTACT<br>";
+                    echo "$PATIENTGENDER<br>";
+                    echo '<form method="post" action="epatient.php">
+                        <input type="submit" value="go back">
+                      </form>';
+                } else {
+                    echo "FAILED TO DELETE DATA<br>";
+                }
+                
+            } catch (mysqli_sql_exception $e) {
+                print "Error!: " . $e->getMessage() . "<br/>";
+                die();
+            }
+        ?>
+    </body>
+</html> 
+
