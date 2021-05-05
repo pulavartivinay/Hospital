564f697a35bed061b3a4c00b0346952d2e8a845e
pulavartivinay <pulavartivinay@gmail.com>
2021-05-06 00:41:00 +0530

    Done billing with alert boxes and functionality

---
+++ billing/billing.php
@@ -0,0 +1,163 @@
+<?php
+    echo '<body style="background-color:#383A59; color:white">';
+    if(array_key_exists('addBilling', $_POST)) {
+        addBilling();
+    }
+    else if(array_key_exists('editBilling', $_POST)) {
+        editBilling();
+    }
+    else if(array_key_exists('deleteBilling', $_POST)) {
+        deleteBilling();
+    }
+    else if(array_key_exists('showBilling', $_POST)) {
+        showBilling();
+    }
+    else if(array_key_exists('searchBilling', $_POST)) {
+        searchBilling();
+    }
+    else if(array_key_exists('service', $_POST)) {
+        service();
+    }
+    function addBilling() {
+        echo '<form method="post" action="add_billing.php">
+                ID: <input type="text" name="id"><br>
+                Type: <input type="text" name="type"><br>
+                Patient_ID: <input type="text" name="patient_id"><br>
+                Amount: <input type="text" name="amount"><br>
+                Date: <input type="text" name="date"><br>
+                Contact: <input type="text" name="contact_number"><br>
+                <input type="submit">
+              </form>';
+    }
+    function editBilling() {
+        echo '<form method="post" action="edit_billing.php">
+                ID: <input type="text" name="id"><br>
+                Type: <input type="text" name="type"><br>
+                Patient_ID: <input type="text" name="patient_id"><br>
+                Amount: <input type="text" name="amount"><br>
+                Date: <input type="text" name="date"><br>
+                Contact: <input type="text" name="contact_number"><br>
+                <input type="submit">
+              </form>';
+    }
+    function deleteBilling() {
+        echo '<form method="post" action="delete_billing.php">
+        		BILLING ID: <input type="text" name="id"><br>
+                
+                <input type="submit" value="commit">
+              </form>';
+    }
+    function showBilling() {
+        try {
+            $username = "root";
+            $password = "";
+            $database = "hospital";
+            $table = "billing";
+            $mysqli = new mysqli("localhost", $username, $password, $database);
+            echo "<h2>BILLING</h2><ol>";
+            $query = "SELECT * FROM billing";
+            $result = $mysqli->query($query);
+            echo '<table border="1" cellspacing="2" cellpadding="2"> 
+                <tr> 
+                    <td> <font face="Monospace">ID</font> </td> 
+                    <td> <font face="Monospace">Type</font> </td> 
+                    <td> <font face="Monospace">Patient_ID</font> </td>
+                    <td> <font face="Monospace">Amount</font> </td> 
+                    <td> <font face="Monospace">Date</font> </td>
+                    <td> <font face="Monospace">Contact Number</font> </td>
+                </tr>';
+            while ($row = $result->fetch_assoc()) {
+                  $field1name = $row["id"];
+                  $field2name = $row["type"];
+                  $field3name = $row["patient_id"];
+                  $field4name = $row["amount"];
+                  $field5name = $row["date"];
+                  $field6name = $row["contact_number"];
+                  echo '<tr> 
+                            <td> <font face="Monospace">'.$field1name.'</td> 
+                            <td> <font face="Monospace">'.$field2name.'</td> 
+                            <td> <font face="Monospace">'.$field3name.'</td> 
+                            <td> <font face="Monospace">'.$field4name.'</td> 
+                            <td> <font face="Monospace">'.$field5name.'</td>
+                            <td> <font face="Monospace">'.$field6name.'</td> 
+                        </tr>';
+            }
+            $result->free();
+            echo "</ol>";
+          } catch (mysqli_sql_exception $e) {
+              print "Error!: " . $e->getMessage() . "<br/>";
+              die();
+          }
+    }
+    function searchBilling() {
+        try {
+            $username = "root";
+            $password = "";
+            $database = "hospital";
+            $table = "billing";
+            $mysqli = new mysqli("localhost", $username, $password, $database);
+            $SEARCHBILLING=$_POST["searchBilling"];
+            echo "<h2>Results for contact number: $SEARCHBILLING</h2><ol>";
+            $query = "SELECT * FROM billing where contact_number LIKE '%".$SEARCHBILLING."%'";
+            $result = $mysqli->query($query);
+            echo '<table border="1" cellspacing="2" cellpadding="2"> 
+                <tr> 
+                    <td> <font face="Monospace">ID</font> </td> 
+                    <td> <font face="Monospace">Payment mode</font> </td> 
+                    <td> <font face="Monospace">Patient_ID</font> </td>
+                    <td> <font face="Monospace">amount</font> </td> 
+                    <td> <font face="Monospace">date</font> </td>
+                    <td> <font face="Monospace">Contact Number</font> </td>
+                </tr>';
+            while ($row = $result->fetch_assoc()) {
+                  $field1name = $row["id"];
+                  $field2name = $row["type"];
+                  $field3name = $row["patient_id"];
+                  $field4name = $row["amount"];
+                  $field5name = $row["date"];
+                  $field6name = $row["contact_number"];
+                  echo '<tr> 
+                            <td> <font face="Monospace">'.$field1name.'</td> 
+                            <td> <font face="Monospace">'.$field2name.'</td> 
+                            <td> <font face="Monospace">'.$field3name.'</td> 
+                            <td> <font face="Monospace">'.$field4name.'</td> 
+                            <td> <font face="Monospace">'.$field5name.'</td> 
+                            <td> <font face="Monospace">'.$field6name.'</td> 
+                        </tr>';
+            }
+            $result->free();
+            echo "</ol>";
+          } catch (mysqli_sql_exception $e) {
+              print "Error!: " . $e->getMessage() . "<br/>";
+              die();
+          }
+    }
+    function service() {
+        try {
+            $username = "root";
+            $password = "";
+            $database = "hospital";
+            $table = "billing";
+            $mysqli = new mysqli("localhost", $username, $password, $database);
+            $SEARCHSERVICE=$_POST["service"];
+            echo "<h2>Results for payment mode: $SEARCHSERVICE</h2><ol>";
+            $query = "call patients_taken_payment('$SEARCHSERVICE')";
+            $result = $mysqli->query($query);
+            echo '<table border="1" cellspacing="2" cellpadding="2"> 
+                <tr> 
+                    <td> <font face="Monospace">Patient ID</font> </td> 
+                </tr>';
+            while ($row = $result->fetch_assoc()) {
+                  $field1name = $row["patient_ID"];
+                  echo '<tr> 
+                            <td> <font face="Monospace">'.$field1name.'</td>
+                        </tr>';
+            }
+            $result->free();
+            echo "</ol>";
+          } catch (mysqli_sql_exception $e) {
+              print "Error!: " . $e->getMessage() . "<br/>";
+              die();
+          }
+    }
+?>
